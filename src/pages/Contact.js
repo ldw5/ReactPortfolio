ed0143aedfdb6fcab8297b565688ee0ba7036ea7
Landrell Williams <dontess5@yahoo.com>
2021-11-22 03:45:31 -0500

    correction to contact me

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
         <h1>Contact Me</h1>
         <ul>
         <li>
-            <a href="mailto:dontess5@yahoo,com"><MdEmail/>dontess5@yahoo.com</a>
+            <a href="mailto:dontess5@yahoo.com"><MdEmail/>dontess5@yahoo.com</a>
           </li>
           <li>
             <a href="tel:4053712743"><MdOutlinePhonelinkRing />405-371-2743</a>
